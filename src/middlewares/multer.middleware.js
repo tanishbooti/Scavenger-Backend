e4ce1e23360646eb042f1d0f2de91313e02e0901
tanishbooti <tanishbooti@gmail.com>
2025-06-25 12:28:43 +0530

    Update multer.middleware.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -4,7 +4,7 @@ import multer from 'multer';
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '/Users/tanishbooti/Desktop/webd/MajorProject/backend/public')
+      cb(null, '/Users/tanishbooti/Desktop/webd/MajorProject/backend/src')
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
